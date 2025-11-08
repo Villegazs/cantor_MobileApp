251e09f79523c4057e8620ffb2ab5a6c22d80639
Villegazs <133931476+Villegazs@users.noreply.github.com>
2025-11-08 00:34:18 -0500

    Implement gem selection and swapping mechanics; add input handling and grid initialization

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,4 +1,15 @@
-﻿public class Gem
-{
+﻿using UnityEngine;
+
+namespace Match3 {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Gem : MonoBehaviour {
+        public GemType type;
+
+        public void SetType(GemType type) {
+            this.type = type;
+            GetComponent<SpriteRenderer>().sprite = type.sprite;
+        }
         
+        public GemType GetType() => type;
+    }
 }
---
+++ Assets/Scripts/GemType.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Match3
+{
+    [CreateAssetMenu(fileName = "NewGemType", menuName = "Match3/Gem Type")]
+    public class GemType : ScriptableObject
+    {
+        public Sprite sprite;
+    }
+}
